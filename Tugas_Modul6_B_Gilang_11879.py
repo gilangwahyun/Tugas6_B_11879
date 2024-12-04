bacb78e9985d36ab670f9ef068abced9a93be796
Gilang Wahyu Nugraha <220711879@students.uajy.ac.id>
2024-12-04 14:54:31 +0700

    Update Tugas_Modul6_B_Gilang_11879.py

--- Tugas_Modul6_B_Gilang_11879.py
+++ Tugas_Modul6_B_Gilang_11879.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-model = load_model(r'C:\Users\lenovo\OneDrive\Documents\Tugas6_B_11879\model_mobilenet.h5')
+model = 'model_mobilenet.h5'
 class_names = ['Matang', 'Mentah']
 
 def classify_image(image_path):
